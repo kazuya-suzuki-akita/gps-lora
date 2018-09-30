61fdc45592445571f1a3ac2ce9e9318c6ccecb79
Kazuya Suzuki <kazuya@ax.jp.nec.com>
2018-09-30 17:41:30 +0900

    Add config procedures for ES920LR.

--- es920lr.py
+++ es920lr.py
@@ -4,11 +4,12 @@ import serial
 import RPi.GPIO as GPIO
 import time
 import struct
+from configparser import ConfigParser
 
 ResetPin = 12
 
 class ES920LR():
-    def __init__(self, dev):
+    def __init__(self, dev, filename="./config.ini"):
         GPIO.setmode(GPIO.BOARD)
         GPIO.setwarnings(False)
         GPIO.setup(ResetPin, GPIO.OUT)
@@ -16,6 +17,37 @@ class ES920LR():
 
         self.dev = dev
         self.serial = serial.Serial(dev, 115200)
+
+        self.readConfig(filename)
+        self.setParameter()
+
+    def readConfig(self, filename):
+        self.config = ConfigParser()
+        self.config.read(filename)
+
+    def setParameters(self):
+        self.sendcmd("2")
+        self.sendcmd("x")
+        self.sendcmd("a 2")
+        self.sendcmd("b %s" % self.config['LoRa']['bw'])
+        self.sendcmd("c %s" % self.config['LoRa']['sf'])
+        self.sendcmd("d %s" % self.config['LoRa']['channel'])
+        self.sendcmd("e %s" % self.config['LoRa']['panid'])
+        self.sendcmd("f %s" % self.config['LoRa']['ownid'])
+        self.sendcmd("g %s" % self.config['LoRa']['dstid'])
+        self.sendcmd("l %s" % self.config['LoRa']['ack'])
+        self.sendcmd("o %s" % self.config['LoRa']['rcvid'])
+        self.sendcmd("p %s" % self.config['LoRa']['rssi'])
+        self.sendcmd("u %s" % self.config['LoRa']['power'])
+        self.sendcmd("z")
+
+    def sendcmd(self, command):
+        self.sendmsg(command)
+        sleep(0.1)
+
+    def sendmsg(self, message):
+        line = "{0}\r\n".format(message).encode('utf-8')
+        self.serial.write(line)
 
     def readline(self, timeout = None):
         if timeout != None:
@@ -28,9 +60,6 @@ class ES920LR():
             self.serial.timeout = None
             self.serial.open()
         return line
-
-    def write(self, msg):
-        self.serial.write(msg.encode('utf-8'))
 
     def parse(self, line):
         fmt = '4s4s4s' + str(len(line) - 14) + 'sxx'
