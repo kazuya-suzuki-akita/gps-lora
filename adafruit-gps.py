94e1a5b12cf0703441047d9df68d682881c80f7d
鈴木一哉 <kazuya-suzuki@akita-pu.ac.jp>
2018-10-03 14:58:39 +0900

    Refactoring

--- adafruit-gps.py
+++ adafruit-gps.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 import serial
-import datetime
+from datetime import date, time
 
 class AdafruitGPS():
     def __init__(self, dev):
         self.serial = serial.Serial(dev, 9600)
-        self.date = datetime.date.today()
+        self.date = date.today()
         self.valid = False
 
     def readline(self, timeout = None):
@@ -33,61 +33,36 @@ class AdafruitGPS():
             latitude *= -1
         return latitude
 
-    def parse_rmc(self, elements):
-        # calculate status
-        if elements[2] == "A":
-            valid = True
-        else:
-            valid = False
+    def parse_time(self, time_string):
+        hour = int(time_string[0:2])
+        minute = int(time_string[2:4])
+        second = int(time_string[4:6])
+        return time(hour, minute, second)
 
-        # calculate latitude
-        self.latitude = self.calc_coordinate(elements[3], elements[4])
-
-        # calculate longitude
-        self.longitude = self.calc_coordinate(elements[5], elements[6])
-
-        # calculate time
-        date_string = elements[9]
+    def parse_date(self, date_string):
         year = 2000 + int(date_string[4:6])
         month = int(date_string[2:4])
         day = int(date_string[0:2])
-        self.date = datetime.date(year, month, day)
+        return date(year, month, day)
 
-        time_string = elements[1]
-        hour = int(time_string[0:2])
-        minute = int(time_string[2:4])
-        second = int(time_string[4:6])
-        self.time = datetime.time(hour, minute, second)
-
-        return self.latitude, self.longitude, self.valid, self.time
+    def parse_rmc(self, elements):
+        self.valid = True if elements[2] == "A" else False
+        self.latitude = self.calc_coordinate(elements[3], elements[4])
+        self.longitude = self.calc_coordinate(elements[5], elements[6])
+        self.date = parse_date(elements[9])
+        self.time = parse_time(elements[1])
 
     def parse_gga(self, elements):
-        # calculate time
-        time_string = elements[1]
-        hour = int(time_string[0:2])
-        minute = int(time_string[2:4])
-        second = int(time_string[4:6])
-        self.time = datetime.time(hour, minute, second)
-
-        # calculate latitude
-        self.latitude = self.calc_coordinate(elements[2], elements[3])
-
-        # calculate longitude
-        self.longitude = self.calc_coordinate(elements[4], elements[5])
-
         # calculate quality
         if int(elements[6]) == 0:
             self.valid = False
-        else:
-            self.valid = True
-
-        # calculate antennna altitude
+            return
+        self.valid = True
+        self.time = parse_time(elements[1])
+        self.latitude = self.calc_coordinate(elements[2], elements[3])
+        self.longitude = self.calc_coordinate(elements[4], elements[5])
         self.altitude = float(elements[9])
-
-        # calculate geoidal separation
         self.separation = float(elements[11])
-
-        return self.latitude, self.longitude, self.valid, self.time, self.altitude, self.separation
 
 def main():
     gps = AdafruitGPS("/dev/ttyUSB0")
@@ -95,12 +70,11 @@ def main():
         line = str(gps.readline(), encoding='utf-8')
         if "$GPRMC" in line:
             elements = line.split(",")
-            latitude, longitude, valid, time = gps.parse_rmc(elements)
-            print(latitude, longitude, sep=',')
+            gps.parse_rmc(elements)
         if "$GPGGA" in line:
             elements = line.split(",")
-            latitude, longitude, valid, time, altitude, separation = gps.parse_gga(elements)
-            print(latitude, longitude, altitude, sep=',')
+            gps.parse_gga(elements)
+        print(gps.latitude, gps.longitude, gps.altitude, gps.valid, sep=',')
 
 if __name__ == "__main__":
     main()
