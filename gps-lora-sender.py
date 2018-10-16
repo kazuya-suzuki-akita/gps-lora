e33ece55d61331b2ae7a712cf534741ffe4264ae
鈴木一哉 <kazuya-suzuki@akita-pu.ac.jp>
2018-10-16 18:10:40 +0900

    スレッド起動順を変更

--- gps-lora-sender.py
+++ gps-lora-sender.py
@@ -7,19 +7,19 @@ from es920lr import ES920LR
 from alivemonitor import AliveMonitor
 
 def main():
-    gps = AdafruitGPS("/dev/ttyS0")
-    lora = ES920LR("/dev/ttyUSB0")
     monitor = AliveMonitor()
+    thread_monitor = threading.Thread(target=monitor.loop)
+    thread_monitor.start()
 
+    gps = AdafruitGPS("/dev/ttyS0")
     thread_gps = threading.Thread(target=gps.loop)
+    thread_gps.start()
+
+    lora = ES920LR("/dev/ttyUSB0")
     thread_sender = threading.Thread(target=lora.send_loop, args=(gps,))
     thread_reciever = threading.Thread(target=lora.recieve_loop)
-    thread_monitor = threading.Thread(target=monitor.loop)
-
-    thread_gps.start()
     thread_sender.start()
     thread_reciever.start()
-    thread_monitor.start()
 
 if __name__ == "__main__":
     main()
