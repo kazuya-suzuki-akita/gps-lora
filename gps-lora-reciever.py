92728c39d3ce3a4bea89f27c0099f305902b407f
鈴木一哉 <kazuya-suzuki@akita-pu.ac.jp>
2018-10-17 10:45:23 +0900

    ログファイルへの出力対応

--- gps-lora-reciever.py
+++ gps-lora-reciever.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python3
 # prototype
 
+import sys
 import threading
+from datetime import datetime
 from es920lr import ES920LR
+
+logprefix = 'lora-sf7-'
 
 def main():
     lora = ES920LR("/dev/ttyUSB0", "./config-USB0.ini")
 
+    now = datetime.now()
+    logfile = now.strftime(logprefix + '%Y%m%d%H%M%S.log')
+    f = open(logfile, 'w')
+    
     while True:
         line = lora.readline()
-        if !line[0:7].isalnum():
+        if not line[0:7].isalnum():
             continue
         rssi, panid, srcid, msg = lora.parse(line)
-        print('{},{},{},{}'.format(rssi, panid, srcid, msg)
+        now = datetime.now()
+        now_str = now.strftime('%Y%m%d%H%M%S')
+        print('{},{},{}'.format(now_str, msg, rssi))
+        f.write('{},{},{}'.format(now_str, msg, rssi))
+        f.flush()
 
 if __name__ == "__main__":
     main()
