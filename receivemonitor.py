04cb85f1df5a9aa5deeb250e1d7578a4fab3187c
鈴木一哉 <kazuya-suzuki@akita-pu.ac.jp>
2018-10-23 18:41:52 +0900

    Lock 削除

--- receivemonitor.py
+++ receivemonitor.py
@@ -13,17 +13,14 @@ class ReceiveMonitor():
         GPIO.setwarnings(False)
         GPIO.setup(ledpin, GPIO.OUT)
 
-        self.lock = threading.Lock()
         self.updated = datetime.now()
 
     def update(self):
-        with self.lock():
-            self.updated = datetime.now()
+        self.updated = datetime.now()
 
     def loop(self):
         while True:
-            with self.lock():
-                delta = ( datetime.now() - self.updated ).total_seconds()
+            delta = ( datetime.now() - self.updated ).total_seconds()
             if delta < 20:
                 GPIO.output(LEDPin, 1)
                 sleep(0.5)
