7c665b9fcb2ea385440d05b3795ab8de5283d484
鈴木一哉 <kazuya-suzuki@akita-pu.ac.jp>
2019-01-31 11:03:24 +0900

    ベースからの送信機能停止

--- gps-lora-base.py
+++ gps-lora-base.py
@@ -12,8 +12,8 @@ def main():
     config.read(sys.argv[1])
     
     lora = ES920LR(config)
-    thread_send = threading.Thread(target=lora.base_send_loop)
-    thread_send.start()
+#    thread_send = threading.Thread(target=lora.base_send_loop)
+#    thread_send.start()
 
     now = datetime.now()
     logprefix = "lora-sf" + config['LoRa']['sf']
